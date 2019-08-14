8b1d701b1ea919491b2eddac2ba3ca35a38173bc
Carolina Moutella <ola@carolinamoutella.com>
2019-08-13 21:44:58 -0300

    Include client details view

--- src/js/components/client_edit_form.js
+++ src/js/components/client_edit_form.js
@@ -4,6 +4,6 @@ Vue.component('client-edit-form', {
   template: '<div class="client-form"><div class="form-container"><p>Editar Cliente</p><form><input type="text" name="endereco" placeholder="Endereço" /><input type="text" name="bairro" placeholder="Bairro" /><input type="text" name="cidade" placeholder="Cidade"/><select name="status"><option selected disabled>Status</option></select><button type="submit">Salvar</button></form></div></div>'
 })
 
-var clientForm = new Vue({
+var clientEditForm = new Vue({
   el: '#client-edit-form'
 })
