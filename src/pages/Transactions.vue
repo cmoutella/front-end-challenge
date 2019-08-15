99c3cad838f16462c786f5e16662f128ec596e55
Carolina Moutella <ola@carolinamoutella.com>
2019-08-15 19:18:33 -0300

    Develop Transactions page

--- src/pages/Transactions.vue
+++ src/pages/Transactions.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Transactions Page</div>
+  <div class="transactions">
+    <div class="container">
+      <p class="transactions-title">Transacional :: Loja da Manu</p>
+      <ul class="transactions-list">
+        <li class="transactions-item transaction">
+          <p class="transaction-date">05/05/2019 14:35:54</p>
+          <p class="transaction-card>">MasterCard</p>
+          <p class="transaction-number">123456*******789</p>
+          <p class="transaction-value">R$ 200,00</p>
+          <div class="transaction-status"></div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
