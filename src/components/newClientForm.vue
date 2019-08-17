0df515ec31d21454261d1f1f2b3e4b027f4df65b
Carolina Moutella <ola@carolinamoutella.com>
2019-08-17 18:20:10 -0300

    create .vue components

---
+++ src/components/newClientForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="client-form">
+    <div class="form-container">
+      <p>Criar Cliente</p>
+      <form>
+        <input type="text" name="nome" placeholder="Nome Fantasia" required />
+        <select name="doc_type">
+          <option selected disabled>Tipo de Documento</option>
+        </select>
+        <input type="text" name="documento" placeholder="Documento" />
+        <input type="text" name="bairro" placeholder="Bairro" />
+        <input type="text" name="cidade" placeholder="Cidade" />
+        <select name="status">
+          <option selected disabled>Status</option>
+        </select>
+        <input type="text" name="nome_socio" placeholder="Nome do Sócio" />
+        <input type="text" name="doc_socio" placeholder="Documento do sócio" />
+        <button type="submit">Salvar</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "create_client_form"
+};
+</script>
