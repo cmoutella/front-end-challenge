a4ae88b4d6a6094c51edd33f8e9d413a7e474cc7
Carolina Moutella <ola@carolinamoutella.com>
2019-08-20 20:33:46 -0300

    Include transactions view

---
+++ src/js/components/transactions.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+Vue.component('transactions', {
+  template: '<div class="transactions"><div class="container"><p class="transactions-title">Transacional :: Loja da Manu</p><ul class="transactions-list"><li class="transactions-item transaction"><p class="transaction-date">05/05/2019 14:35:54</p><p class="transaction-card>">MasterCard</p><p class="transaction-number">123456*******789</p><p class="transaction-value">R$ 200,00</p><div class="transaction-status"></div></li></ul></div></div>'
+})
+
+var login = new Vue({
+  el: "#transactions"
+})
